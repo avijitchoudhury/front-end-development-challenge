39bf40eeb1a4a85fbf0b5cb86e8146b09edf00d5
Avijit Choudhury <avijit.choudhury24@gmail.com>
2020-02-17 19:39:35 -0500

    fetched data from API and rendered to HTML

---
+++ src/index.js
@@ -0,0 +1,83 @@
+import _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+function fetchData1 () {
+  fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=GOOGL&apikey=JQ1LE8WZCWJRAMXL_').then(response => {
+  if(!response.ok) {
+    throw Error("ERROR")
+  }
+  return response.json();
+  }).then(data => {
+    let payload = Object.values(data);
+    let objPayload = Object.values(payload[0]);
+  
+    for(let i = 0; i < objPayload.length; i++) {
+      if (objPayload[8] > 0) {
+        return document.querySelector('#app1').insertAdjacentHTML("afterbegin", `Price: ${objPayload[4]} <br> Increase: ${objPayload[8]}`) 
+      } else {
+        return document.querySelector('#app1').insertAdjacentHTML("afterbegin", `Price: ${objPayload[4]} <br> Decrease: ${objPayload[8]}`) 
+      }
+    }
+  }).catch(error => {
+    console.log(error)
+  });
+}
+
+function fetchData2() {
+  fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=MSFT&apikey=JQ1LE8WZCWJRAMXL_').then(response => {
+    if (!response.ok) {
+      throw Error("ERROR")
+    }
+    return response.json();
+  }).then(data => {
+    let payload = Object.values(data);
+    let objPayload = Object.values(payload[0]);
+
+    for (let i = 0; i < objPayload.length; i++) {
+      if (objPayload[8] > 0) {
+        return document.querySelector('#app2').insertAdjacentHTML("afterbegin", `Price: ${objPayload[4]} <br> Increase: ${objPayload[8]}`)
+      } else {
+        return document.querySelector('#app2').insertAdjacentHTML("afterbegin", `Price: ${objPayload[4]} <br> Decrease: ${objPayload[8]}`)
+      }
+    }
+  }).catch(error => {
+    console.log(error)
+  });
+}
+
+
+function fetchData3() {
+  fetch('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=BTCUSD&apikey=JQ1LE8WZCWJRAMXL_').then(response => {
+    if (!response.ok) {
+      throw Error("ERROR")
+    }
+    return response.json();
+  }).then(data => {
+    let payload = Object.values(data);
+    let objPayload = Object.values(payload[0]);
+
+    for (let i = 0; i < objPayload.length; i++) {
+      if (objPayload[8] > 0) {
+        return document.querySelector('#app3').insertAdjacentHTML("afterbegin", `Price: ${objPayload[4]} <br> Increase: ${objPayload[8]}`)
+      } else {
+        return document.querySelector('#app3').insertAdjacentHTML("afterbegin", `Price: ${objPayload[4]} <br> Decrease: ${objPayload[8]}`)
+      }
+    }
+  }).catch(error => {
+    console.log(error)
+  });
+}
+
+fetchData1();
+fetchData2();
+fetchData3();
